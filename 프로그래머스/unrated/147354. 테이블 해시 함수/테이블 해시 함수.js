c65cc24b40894d985f615ed98b5dc22a02ecc73e
sunnie-han <91239986+sunnie-han@users.noreply.github.com>
2023-04-05 20:54:12 +0900

    [unrated] Title: 테이블 해시 함수, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.js
+++ 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.js
@@ -5,29 +5,7 @@ function solution(data, col, row_begin, row_end) {
     let answer = 0
     for (let i = row_begin; i <= row_end; i++) {
         const s = data[i - 1].reduce((sum, cur) => sum + cur % i, 0)
-        answer = xor(answer, s)
-    }
-    
-    return answer
-}
-
-const xor = (a, b) => {
-    let answer = 0
-    
-    let bitA = 0
-    let bitB = 0
-    
-    let exponent = 0
-    
-    while (!(a === 0 && b === 0)) {
-        bitA = a % 2
-        bitB = b % 2
-        if (bitA != bitB) {
-            answer += Math.pow(2, exponent)
-        }
-        exponent++
-        a = Math.floor(a / 2)
-        b = Math.floor(b / 2)
+        answer ^= s
     }
     
     return answer
