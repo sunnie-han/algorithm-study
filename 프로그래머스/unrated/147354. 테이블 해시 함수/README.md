a61205b471ebf46d7bc6a4717fdc67b157ed886e
sunnie-han <91239986+sunnie-han@users.noreply.github.com>
2023-04-05 21:04:27 +0900

    [unrated] Title: 테이블 해시 함수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/147354. 테이블 해시 함수/README.md
+++ 프로그래머스/unrated/147354. 테이블 해시 함수/README.md
@@ -1,10 +1,10 @@
 # [unrated] 테이블 해시 함수 - 147354 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/147354?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/147354) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.10 ms
+메모리: 33.4 MB, 시간: 0.07 ms
 
 ### 구분
 
--- 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.js
+++ 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.js
@@ -1,7 +1,6 @@
 function solution(data, col, row_begin, row_end) {
     const colIndex = col - 1
-    const sortDataByKey = data.sort((a, b) => b[0] - a[0])
-    const sortDataByCol = sortDataByKey.sort((a, b) => a[colIndex] - b[colIndex])
+    data.sort((a, b) => a[colIndex] - b[colIndex] || b[0] - a[0])
     let answer = 0
     for (let i = row_begin; i <= row_end; i++) {
         const s = data[i - 1].reduce((sum, cur) => sum + cur % i, 0)
