5586963985fadaa1c467513d9716b19d6bffe992
sunnie-han <91239986+sunnie-han@users.noreply.github.com>
2023-09-20 23:13:11 +0900

    [level 1] Title: 체육복, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -1,29 +1,11 @@
 function solution(n, lost, reserve) {
-    const note = Array(n+2).fill(0);
-    
+    const note = Array(n+1).fill(1);
+    note[0] = 0;
     lost.forEach((student) => {
         note[student] -= 1;
     });
     reserve.forEach((student) => {
         note[student] += 1;
     });
-    
-    let answer = n;
-    
-    for (let student = 1; student < n+1; student++) {
-        const cur = note[student];
-        if (cur < 0) {
-            const hasBefore = note[student - 1] > 0;
-            if (hasBefore) {
-                continue;
-            }
-            const hasNext = note[student + 1] > 0;
-            if (hasNext) {
-                note[student + 1] -= 1;
-                continue;
-            }
-            answer -= 1;
-        }
-    }
-    return answer;
+    console.log(note)
 }
