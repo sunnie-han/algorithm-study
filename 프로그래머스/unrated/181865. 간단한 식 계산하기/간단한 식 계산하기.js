19a3b825980baf320458d30a973e4231204951a4
sunnie-han <91239986+sunnie-han@users.noreply.github.com>
2023-10-28 21:53:01 +0900

    [level unrated] Title: 간단한 식 계산하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -0,0 +1,11 @@
+function solution(binomial) {
+    const [a, op, b] =  binomial.split(' ');
+    switch (op) {
+        case '+':
+            return Number(a) + Number(b);
+        case '-':
+            return Number(a) - Number(b);
+        case '*':
+            return Number(a) * Number(b);
+    }
+}
