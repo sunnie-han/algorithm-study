122db24e65f4ff1e82110bc113a78e68316853f9
sunnie-han <91239986+sunnie-han@users.noreply.github.com>
2023-10-28 22:27:28 +0900

    [level unrated] Title: 0 떼기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.js
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.js
@@ -1,10 +1,3 @@
 function solution(n_str) {
-    for (let i=0; i<n_str.length; i++) {
-        if (n_str[i] === '0') {
-            continue;
-        } else {
-            return n_str.slice(i);
-        }
-    }
-    return n_str;
+    return String(Number(n_str))
 }
