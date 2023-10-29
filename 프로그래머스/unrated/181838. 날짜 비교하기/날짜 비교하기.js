2ccd52fd2d5017738503a2e2e365d271ec2627aa
sunnie-han <91239986+sunnie-han@users.noreply.github.com>
2023-10-29 12:45:57 +0900

    [level unrated] Title: 날짜 비교하기, Time: 0.11 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,21 +1,3 @@
 function solution(date1, date2) {
-    const compare = ([y1, m1, d1], [y2, m2, d2]) => {
-        if (y1 > y2) {
-            return 0;
-        }
-        if (y1 < y2) {
-            return 1;
-        }
-        if (m1 > m2) {
-            return 0;
-        }
-        if (m1 < m2) {
-            return 1;
-        }
-        if (d1 >= d2) {
-            return 0;
-        }
-        return 1;
-    }
-    return compare(date1, date2);
+    return new Date(date1) < new Date(date2) ? 1 : 0;
 }
